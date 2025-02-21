d319d3dbf489c1312f26483fe4bf780fd9b097df
Eli Klein <eliilek@gmail.com>
2025-02-21 12:26:51 -0800

    Separate workers

--- docker/nluaba-backend/worker-entrypoint.sh
+++ docker/nluaba-backend/worker-entrypoint.sh
@@ -9,4 +9,4 @@ export CLOUDINARY_URL=$(cat /run/secrets/CLOUDINARY_URL)
 export S3_ACCESS=$(cat /run/secrets/S3_ACCESS)
 export S3_SECRET=$(cat /run/secrets/S3_SECRET)
 
-python manage.py rqworker default
+python manage.py rqworker nluaba
