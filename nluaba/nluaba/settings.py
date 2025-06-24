45927a54cbd043e3803991b7e2277f416709ca10
Eli Klein <eliilek@gmail.com>
2025-06-23 20:33:50 -0700

    Expose ports for email

--- nluaba/nluaba/settings.py
+++ nluaba/nluaba/settings.py
@@ -226,8 +226,8 @@ LOGGING = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False)
+EMAIL_USE_TLS = bool(os.getenv('EMAIL_USE_TLS', True))
+EMAIL_USE_SSL = bool(os.getenv('EMAIL_USE_SSL', False))
