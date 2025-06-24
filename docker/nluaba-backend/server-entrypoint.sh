953148c58813ec867d7a7228dd7adac7743dd312
Eli Klein <eliilek@gmail.com>
2025-06-23 19:55:20 -0700

    I am an idiot

--- docker/nluaba-backend/server-entrypoint.sh
+++ docker/nluaba-backend/server-entrypoint.sh
@@ -18,8 +18,4 @@ done
 
 python manage.py collectstatic --noinput
 
-export CLOUDINARY_URL=$(cat /run/secrets/CLOUDINARY_URL_OLD)
-export S3_ACCESS=$(cat /run/secrets/S3_ACCESS)
-export S3_SECRET=$(cat /run/secrets/S3_SECRET)
-
 gunicorn -b 0.0.0.0 -p 8000 nluaba.wsgi
--- docker/nluaba-backend/worker-entrypoint.sh
+++ docker/nluaba-backend/worker-entrypoint.sh
@@ -5,8 +5,4 @@ do
     echo "Waiting for server volume..."
 done
 
-export CLOUDINARY_URL=$(cat /run/secrets/CLOUDINARY_URL)
-export S3_ACCESS=$(cat /run/secrets/S3_ACCESS)
-export S3_SECRET=$(cat /run/secrets/S3_SECRET)
-
 python manage.py rqworker nluaba
