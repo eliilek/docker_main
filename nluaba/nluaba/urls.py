696f4c28766efa76f8437cb231c4806b872ace7f
Eli Klein <eliilek@gmail.com>
2025-08-01 12:28:41 -0700

    URL updates to catch /

--- nluaba/nluaba/urls.py
+++ nluaba/nluaba/urls.py
@@ -1,19 +1,3 @@
-"""safmeds URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
@@ -32,4 +16,8 @@ urlpatterns += [
     re_path(r'realexams/', include('realexams.urls')),
     re_path(r'exams/', include('exams.urls')),
     re_path(r'thesis/', include('thesis_readiness.urls')),
+]
+
+urlpatterns += [
+    re_path(r'.*', views.splash, name='splash'),
 ]
