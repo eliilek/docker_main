e52a60f459db1226a3158ae83bdf60fb3cd270f2
Eli Klein <eliilek@gmail.com>
2025-08-04 17:17:32 -0700

    Update domains in script, update nginx to match wildcard for challenge

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(kleinapp.com www.kleinapp.com)
+domains=(kleinapp.com *.kleinapp.com)
 rsa_key_size=4096
 data_path="./docker/certbot"
 email="eliilek@gmail.com" # Adding a valid address is strongly recommended
