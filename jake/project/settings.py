63c3b452c4b1697703b3b92891dae11233085020
Eli Klein <eliilek@gmail.com>
2025-10-14 17:33:27 -0700

    Forgot there were additional email config steps in settings.py

--- jake/project/settings.py
+++ jake/project/settings.py
@@ -202,3 +202,11 @@ LOGIN_URL = '/accounts'
 LOGIN_REDIRECT_URL = '/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = bool(os.getenv('EMAIL_USE_TLS', True))
+EMAIL_USE_SSL = bool(os.getenv('EMAIL_USE_SSL', False))
